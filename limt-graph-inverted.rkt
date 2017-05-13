3b573c29466ace6c34fa906c0fc239aef20bb8a3
mathwiz <mathwiz@gmail.com>
2017-05-13 15:09:55 -0400

    all graphs

--- limt-graph-inverted.rkt
+++ limt-graph-inverted.rkt
@@ -7,35 +7,36 @@
 (define (seq n) (seq-fn n values))
 (define (seq-inv n) (seq-fn n (lambda (n) (if (= n 0) 0 (/ (- 1) n)))))
 
-(define (cartesian fn)
+(define (cartesian n fn)
   (parameterize ([plot-width    150]
                  [plot-height   150]
                  [plot-x-label  #f]
                  [plot-y-label  #f])
-    (define xs (seq-inv 16))
-    (define ys (seq-fn 16 fn))
+    (define xs (seq-inv n))
+    (define ys (seq-fn n fn))
     (plot (points (map vector xs ys) #:size 3))))
 
 ;; Group I
 (list
- (cartesian (lambda (n) (if (= n 0) 0 (* (expt (- 1) n) (/ 1 n)))))
- (cartesian (lambda (n) (if (= n 0) 0 (/ (- n 1) n))))
- (cartesian (lambda (n) (if (= n 0) 0 (expt (- (/ 1 2)) n))))
- (cartesian (lambda (n) (if (= n 0) 0 (+ 1 (/ 1 n)))))
+ (cartesian 16 (lambda (n) (if (= n 0) 0 (* (expt (- 1) n) (/ 1 n)))))
+ (cartesian 16 (lambda (n) (if (= n 0) 0 (/ (- n 1) n))))
+ (cartesian 16 (lambda (n) (if (= n 0) 0 (expt (- (/ 1 2)) n))))
+ (cartesian 16 (lambda (n) (if (= n 0) 0 (+ 1 (/ 1 n)))))
+ (cartesian 64 (lambda (n) (if (= n 0) 0 (/ 1 n))))
  )
 
 ;; Group II
 (list
- (cartesian (lambda (n) n))
- (cartesian (lambda (n) (- n)))
- (cartesian (lambda (n) (* n n)))
- (cartesian (lambda (n) (- n 10)))
+ (cartesian 16 (lambda (n) n))
+ (cartesian 16 (lambda (n) (- n)))
+ (cartesian 16 (lambda (n) (* n n)))
+ (cartesian 16 (lambda (n) (- n 10)))
  )
 
 ;; Group III
 (list
- (cartesian (lambda (n) (expt (- 1) n)))
- (cartesian (lambda (n) (if (= n 0) 0 (* (expt (- 1) n) (/ (- n 1) n)))))
- (cartesian (lambda (n) (* (expt (- 1) n) n)))
- (cartesian (lambda (n) (expt (- 2) n)))
+ (cartesian 16 (lambda (n) (expt (- 1) n)))
+ (cartesian 16 (lambda (n) (if (= n 0) 0 (* (expt (- 1) n) (/ (- n 1) n)))))
+ (cartesian 16 (lambda (n) (* (expt (- 1) n) n)))
+ (cartesian 16 (lambda (n) (expt (- 2) n)))
  )
