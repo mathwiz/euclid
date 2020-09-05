f30577b47e246e441eeade2f2018fd8669129cf5
Yohan Lee <yohanlee@yahoo.com>
2020-09-05 13:22:45 -0400

    pi

--- compute-pi.rkt
+++ compute-pi.rkt
@@ -15,3 +15,27 @@
 
 (define (pi-error terms)
   (- pi (compute-pi terms)))
+
+
+(define (string->number/binary x)
+  (string->number x 2))
+
+
+(define (number->string/binary x)
+  (define decimals-places 10)
+  (define digits-all (~r (inexact->exact (round (* x (expt 2 decimals-places))))
+                         #:base 2
+                         #:pad-string "0"
+                         #:min-width (add1 decimals-places)))
+  (define digits-length (string-length digits-all))
+  (define integer-part (substring digits-all 0 (- digits-length decimals-places)))
+  (define decimal-part* (string-trim (substring digits-all (- digits-length decimals-places))
+                                    "0" 
+                                    #:left? #f
+                                    #:repeat? #t))
+  (define decimal-part (if (string=? decimal-part* "") "0"  decimal-part*))
+  (string-append integer-part "." decimal-part))
+
+
+(define (pi-binary terms)
+  (string->number (number->string/binary (compute-pi terms))))
