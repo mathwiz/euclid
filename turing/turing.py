93d7a7c8c74972e723cb73ccb64fa393e9ab7be4
Yohan Lee <yohanlee@yahoo.com>
2021-09-01 14:14:49 -0400

    turing

--- turing/turing.py
+++ turing/turing.py
@@ -4,12 +4,16 @@ class Machine:
         self.init_states(states)
         self.init_symbols(symbols)
         self.init_quintuples()
-        self.state = 'start'
+        self.init_machine()
 
     def __repr__(self):
         quintuples = list(self.quintuples.values())
         quintuples.sort()
         return str(quintuples)
+
+    def init_machine(self):
+        self.pos = 0
+        self.state = 'start'
 
     def init_moves(self):
         self.moves = { -1, 0, 1 }
@@ -50,17 +54,25 @@ class Machine:
 
     def set_tape(self, tapeString):
         self.tape = list(tapeString)
-        self.pos = 0
+        self.init_machine()
 
     def get_tape(self):
         return "".join(self.tape)
 
+    def get_state(self):
+        return self.state
+
+    def get_position(self):
+        return self.pos
+
     def step(self):
         if self.state != 'end':
-            q = self.get_quintuple(self.state, tape[self.pos])
+            q = self.get_quintuple(self.state, self.tape[self.pos])
             self.tape[self.pos] = q[2]
             self.change_tape(q[3])
             self.state = q[4]
+            return True
+        return False
 
     def change_tape(self, move):
         newpos = self.pos + move
@@ -71,12 +83,10 @@ class Machine:
             self.tape.insert(newpos, ' ')
         self.pos = newpos
 
-        
-
     def exec_all(self):
-        while self.state != 'end':
-            self.step()
-            
+        while self.step():
+            pass
+        return self.get_tape()
         
 
 
@@ -86,7 +96,21 @@ class AddingMachine(Machine):
 
 
 class AddOneDigit(Machine):
-    def __init__(self, states, symbols):
+    def __init__(self, states = ['prescan', 'scan', 'add0', 'add1'], symbols = ['+']):
         Machine.__init__(self, states, symbols)
+        self.add_tuple(['start', '1', '1', 1, 'prescan'])
+        self.add_tuple(['start', '0', '0', 1, 'prescan'])
+        self.add_tuple(['prescan', '0', '0', 1, 'prescan'])
+        self.add_tuple(['prescan', '1', '1', 1, 'prescan'])
+        self.add_tuple(['prescan', '+', '+', 1, 'prescan'])
+        self.add_tuple(['prescan', ' ', ' ', -1, 'scan'])
+        self.add_tuple(['scan', '1', ' ', -1, 'add1'])
+        self.add_tuple(['scan', '0', ' ', -1, 'add0'])
+        self.add_tuple(['add0', '+', ' ', -1, 'end'])
+        self.add_tuple(['add1', '+', ' ', -1, 'add1'])
+        self.add_tuple(['add1', '0', '1', 1, 'end'])
+        self.add_tuple(['add1', ' ', '1', -1, 'end'])
+        self.add_tuple(['add1', '1', '0', -1, 'add1'])
+             
 
 
--- turing/turing_test.py
+++ turing/turing_test.py
@@ -2,18 +2,45 @@ import unittest
 import turing
 
 
-m = turing.Machine(['scan', 'add1', 'add0'], ['+'])
-m.add_tuple(['start', ' ', ' ', -1, 'scan'])
-m.add_tuple(['scan', '1', '1', -1, 'add1'])
-m.add_tuple(['scan', '0', '0', -1, 'add0'])
-m.add_tuple(['add0', '+', ' ', -1, 'end'])
-m.add_tuple(['add1', '+', ' ', -1, 'add1'])
-m.add_tuple(['add1', '0', '1', 1, 'end'])
-m.add_tuple(['add1', ' ', '1', -1, 'end'])
-m.add_tuple(['add1', '1', '0', -1, 'add1'])
-print(m)
-
+# m = turing.Machine(['scan', 'add1', 'add0'], ['+'])
+# m.add_tuple(['start', ' ', ' ', -1, 'scan'])
+# m.add_tuple(['scan', '1', '1', -1, 'add1'])
+# m.add_tuple(['scan', '0', '0', -1, 'add0'])
+# m.add_tuple(['add0', '+', ' ', -1, 'end'])
+# m.add_tuple(['add1', '+', ' ', -1, 'add1'])
+# m.add_tuple(['add1', '0', '1', 1, 'end'])
+# m.add_tuple(['add1', ' ', '1', -1, 'end'])
+# m.add_tuple(['add1', '1', '0', -1, 'add1'])
+# print(m)
 
 # test validations
-m.add_tuple(['start', ' ', ' ', -1, 'PreScan'])
+# m.add_tuple(['start', ' ', ' ', -1, 'PreScan'])
+
+def run_tape(machine, tape):
+    machine.set_tape(tape)
+    show_state(machine, tape)
+    while machine.step():
+        show_state(machine, tape)
+    print("final tape:" + machine.get_tape() + ":")
+    print("")
+
+def show_state(machine, tape):
+    print((' ' * machine.get_position()) + '*')
+    print(machine.get_tape())
+    
+
+incrementer = turing.AddOneDigit()
+# print(incrementer)
+
+atape = '1+1'
+print('tape:' + atape)
+run_tape(incrementer, atape)
+
+atape = '11+1'
+print('tape:' + atape)
+run_tape(incrementer, atape)
+
+atape = '1011+1'
+print('tape:' + atape)
+run_tape(incrementer, atape)
 
