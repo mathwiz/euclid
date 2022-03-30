db73494d0c6f426bd0de726a44665aac8d1093c8
Yohan Lee <yohanlee@yahoo.com>
2022-03-30 17:21:03 -0400

    binary conversion

--- calculator/binary.lisp
+++ calculator/binary.lisp
@@ -4,8 +4,15 @@
 
 (let ((d 0.11001001))
   (sigma i 0 7 1 
-    (if (= (truncate (* (abs d) 10)) 1)
-        1
+    (if (= (mod (truncate (* (abs d) (expt 10 (1+ i)))) 10) 1)
+        (expt 2 (- 7 i))
+        0)
+))
+
+(defun b-to-d (d)
+  (sigma i 0 7 1 
+    (if (= (mod (truncate (* (abs d) (expt 10 (1+ i)))) 10) 1)
+        (expt 2 (- 7 i))
         0)
 ))
 
