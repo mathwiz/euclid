d166dd80b19205bc31077d008571b17c6260d30d
Yohan Lee <yohanlee@yahoo.com>
2022-04-01 10:07:45 -0400

    binary

--- calculator/binary.lisp
+++ calculator/binary.lisp
@@ -18,7 +18,7 @@
 
 (defun blist-to-d (digits)
   (labels ((recur (l i acc)
-      (cond ((null l) acc) 
+      (cond ((null l) acc)
             ((= 0 (car l)) (recur (cdr l) (1+ i) acc))
             ('t (recur (cdr l) (1+ i) (+ (expt 2 i) acc))))))
     (recur (reverse digits) 0 0)))
@@ -26,7 +26,7 @@
 (defun b-to-d (b)
   (sigma i 0 7 1 (if (= (mod (truncate (* (abs b)
                                           (expt 10 (1+ i)))) 10) 1)
-                     (expt 2 (+ (* 8 (truncate b)) (- 7 i))) 
+                     (expt 2 (+ (* 8 (truncate b)) (- 7 i)))
                      0)))
 
 (defun d-to-b (d)
@@ -34,19 +34,22 @@
 (*
  (expt 10 0)
  (sigma i 0 7 1
-   (let ((foo (expt 2.0 (- 7 i)))) 
-     (if (= (truncate (/ (abs (g 'd)) 
-                         (l 'f (expt 2.0 (- 7 i)) ))) 
-            1) 
-         (+ (* 0 (l 'd (- (g 'd) (g 'f)))) 
+   (let ((foo (expt 2.0 (- 7 i))))
+     (if (= (truncate (/ (abs (g 'd))
+                         (l 'f (expt 2.0 (- 7 i)) )))
+            1)
+         (+ (* 0 (l 'd (- (g 'd) (g 'f))))
             (expt 10.0 (- (1+ i))))
          0)))))
 
-(defun byte-height (d)
-  (+
-   (* 0 (l 'd (truncate (abs d))))
-   (if (> (g 'd) (expt 2 32))
-       -1
-       (if 0
-           1
-           2))))
+(defun byte-height (d) 
+  (+ (* 0 (l 'd (truncate (abs d)))) 
+     (if (< (g 'd) 
+            (expt 2 32)) 
+         (if (< (g 'd) 
+                (expt 2 24)) 
+             (if (< (g 'd)
+                    (expt 2 16))
+                 (if (< (g 'd)
+                        (expt 2 8))
+                     0 1) 2) 3) -1)))
