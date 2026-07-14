159a36da1fe846788805720175eec1af8075d88c
Yohan Lee <yohanlee@yahoo.com>
2026-07-14 13:06:43 -0400

    more shadows

--- multivariable_calculus/ch11/deg2rad.m
+++
@@ -1,4 +0,0 @@
-function VAL = deg2rad(theta)
-  d = rem(theta, 360);
-  VAL = d * pi / 180;
-endfunction
--- multivariable_calculus/ch11/rad2deg.m
+++
@@ -1,4 +0,0 @@
-function VAL = rad2deg(theta)
-  t = rem(theta, 2*pi);
-  VAL = t * 180 / pi;
-endfunction
