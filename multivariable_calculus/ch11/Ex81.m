ce3b17fd1dc2832ac7ce4d4bf2c6a598de795734
Yohan Lee <yohanlee@yahoo.com>
2026-07-16 13:54:44 -0400

    Ex81

--- multivariable_calculus/ch11/Ex81.m
+++ multivariable_calculus/ch11/Ex81.m
@@ -1,7 +1,41 @@
-function Ex81(deg)
-  result = helper(deg);
-  printf("magnitude: %f; degrees: %f\n",
-         result(1), rad2deg(result(2)));
+function Ex81
+  print_result(0)
+  print_result(30)
+  print_result(60)
+  print_result(90)
+  print_result(120)
+  print_result(150)
+  print_result(180)
+  
+  plot_helper()
+endfunction
+
+function print_result(angle)
+  result = helper(angle);
+  printf("angle: %f; magnitude: %f; degrees: %f\n",
+         angle, result(1), rad2deg(result(2)));
+endfunction
+
+function plot_helper()
+  x = linspace(0, 180, 90);
+  mags = results(x, 1);
+  alphas = results(x, 2);
+  hold off;
+  [ax, h1, h2] = plotyy(x, mags, x, rad2deg(alphas));
+  xlabel("Degrees");
+  ylabel(ax(1), "Magnitude (Newtons)");
+  ylabel(ax(2), "Direction Angle (Degrees)");
+  title("Magnitude and Direction by Angle Between Vectors");
+  printf("Plot finished.\n");
+endfunction
+
+function VAL = results(angles, index)
+  vals = [];
+  for theta = angles
+    polar = helper(theta);
+    vals = [ vals, polar(index) ];
+  end
+  VAL = vals;
 endfunction
 
 function VAL = helper(theta)
