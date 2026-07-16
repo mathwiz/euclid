073dbe06d2dc3f1740cbc0b207bbc57f1144f0f2
Yohan Lee <yohanlee@yahoo.com>
2026-07-16 12:39:31 -0400

    Ex81

--- multivariable_calculus/ch11/Ex81.m
+++ multivariable_calculus/ch11/Ex81.m
@@ -1,12 +1,12 @@
-function Ex81
-  result = helper(30);
+function Ex81(deg)
+  result = helper(deg);
   printf("magnitude: %f; degrees: %f\n",
          result(1), rad2deg(result(2)));
 endfunction
 
 function VAL = helper(theta)
-  f1 = pol2comp(275, deg2rad(0))
-  f2 = pol2comp(180, deg2rad(30))
-  w = f1 + f2
-  VAL = comp2pol(w)
+  f1 = pol2comp(275, deg2rad(0));
+  f2 = pol2comp(180, deg2rad(theta));
+  w = f1 + f2;
+  VAL = comp2pol(w);
 endfunction
